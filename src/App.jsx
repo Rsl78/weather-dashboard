4545368b5ffe0ebbbd132a56b4f8cfb6324dba43
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2025-01-22 17:51:30 +0600

    weather context and provider related work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import { WeatherProvider } from "./provider";
 
 export default function App() {
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
+    <WeatherProvider>
+      <div className="grid place-items-center h-screen">
+        <Header />
 
-      <main>
-        <section>
-          <WeatherBoard/>
-        </section>
-      </main>
-    </div>
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 }
--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -1,23 +1,28 @@
+import { useContext } from "react";
 import AddToFavourite from "./AddToFavourite";
 import WeatherCondition from "./WeatherCondition";
 import WeatherHeadline from "./WeatherHeadline";
-
-import { useWeather } from "../../hooks";
+import { WeatherContext } from "../../context";
 
 const WeatherBoard = () => {
-  const {loading, error, weatherData} = useWeather();
-  console.log(loading, error, weatherData);
-    return (
-      <div className="container">
-        <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
-          <div className="grid md:grid-cols-2 gap-10 md:gap-6">
-            <AddToFavourite/>
-            <WeatherHeadline/>
-            <WeatherCondition/>
-          </div>
+  const { weatherData, loading } = useContext(WeatherContext);
+  return (
+    <div className="container">
+      <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[520px] max-w-[1058px] mx-auto">
+        <div className="grid md:grid-cols-2 gap-10 md:gap-6">
+          {loading.state ? (
+            <div className="text-white text-2xl">{loading.message}</div>
+          ) : (
+            <>
+              <AddToFavourite />
+              <WeatherHeadline />
+              <WeatherCondition />
+            </>
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default WeatherBoard;
