833ebbb592742780303bfa58d84416aa21205160
Md Roknuzzaman Rasel <rasel@graduate.utm.my>
2025-01-23 13:00:48 +0600

    add to favourite functionality added

--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,12 +1,37 @@
+import { useContext, useState, useEffect} from "react";
 import HeartIcon from "../../assets/heart.svg";
+import RedHeartIcon from "../../assets/heart-red.svg";
+import { FavouriteContext, WeatherContext } from "../../context";
 
 const AddToFavourite = () => {
+  const { addToFavourites, removeFromFavourites, favourites } = useContext(FavouriteContext);
+  const {weatherData} = useContext(WeatherContext);
+  const [isFavourite, toggleFavourite] = useState(false);
+  const {latitude, longitude, location} = weatherData;
+
+  useEffect(() => {
+    const found = favourites.find((fav) => fav.location === location);
+    toggleFavourite(found);
+  },[])
+  
+  const handleFavourites = () => {
+    const found = favourites.find(fav => fav.location === location);
+
+    if(!found){
+      addToFavourites(latitude, longitude, location);
+    }else{
+      removeFromFavourites(location);
+    }
+    toggleFavourite(!isFavourite);
+  }
+
     return (
       <div className="md:col-span-2">
         <div className="flex items-center justify-end space-x-6">
-          <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
-            <span>Add to Favourite</span>
-            <img src={HeartIcon} alt="" />
+          <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]"
+          onClick={handleFavourites}>
+            <span>{isFavourite? "Remove From Favourite": "Add to Favourite"}</span>
+            <img src={isFavourite? RedHeartIcon :HeartIcon } alt="" />
           </button>
         </div>
       </div>
